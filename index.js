dd8c8dfd34fce5bf726f9b4a185fbfdca890b806
Hannah Drury <hannah_drury@yahoo.com>
2020-01-06 19:35:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,12 +9,49 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(item) 
+{
+  var itemObject = {};
+  
+  itemObject["itemName"] = item;
+  
+  var randNum = Math.floor(Math.random() * 100) + 1;
+  
+  itemObject["itemPrice"] = randNum;
+  
+  cart.push(itemObject);
+  
+  return `${item} has been added to your cart.`;
 }
 
-function viewCart() {
-  // write your code here
+function viewCart() 
+{
+    var listA = "";
+    
+    if (cart.length === 1) // this loop is only for if there is one item in the array 
+    {
+        for(var i = 0; i < cart.length; i++)
+      {
+        listA = `${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}.`;
+      }
+    
+      return `In your cart, you have ${listA}.`;
+      
+    }
+    
+    if (cart.length > 1) // returns the last item with an and
+    {
+      for(i = 0; i < cart.length-1; i++)
+      {
+        listA = `${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}.`;
+      }
+    
+    }
+    else
+    {
+      return "Your shopping cart is empty.";
+    }
+    
 }
 
 function total() {
